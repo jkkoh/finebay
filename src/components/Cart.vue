83d842e6c27a218d9efabbab40559e6f88e5240a
jkkoh <jeongkyu7@gmail.com>
2023-06-17 06:54:10 +0900

    updated cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -2,7 +2,40 @@
     <div class="wrap">
         <div class="main_wrap">
             <div class="outer_text_area">
-                <span class="material-icons md-48">shopping_cart</span> <p class="inner_text">Cart</p>
+                <div class="top_outer_text">
+                    <div class="cart_icon">
+                        <span class="material-icons md-48">shopping_cart</span>
+                    </div>
+                    <div class="cart_text">
+                        <span class="inner_text">Cart</span>
+                    </div>
+                </div>
+                <div class="bot_outer_text">
+                    <div class="check_box_pos"> 
+                        <input type="checkbox" class="check_box">
+                    </div>
+                    <div class="image_box_pos">
+                        <span>이미지</span>
+                    </div>
+                    <div class="info_box_pos">
+                        <span>상품정보</span>
+                    </div>
+                    <div class="price_box_pos">
+                        <span>판매가</span>
+                    </div>
+                    <div class="quantity_box_pos">
+                        <span>수량</span>
+                    </div>
+                    <div class="delievery_fee_box_pos">
+                        <span>배송비</span>
+                    </div>
+                    <div class="actual_price_box_pos">
+                        <span>결제 금액</span>
+                    </div>
+                    <div class="choose_box_pos">
+                        <span>삭제</span>
+                    </div>
+                </div>
             </div>
             <div v-for="(item, index) in displayCart" :key="index">
                 <div v-if="item.quantity[0]" class="outer_product">
@@ -10,26 +43,60 @@
                         <div class="check_box_pos"> <input type="checkbox" class="check_box"></div>
                         <div class="image_box_pos"> <img :src="item.image" class="inner_img" alt=""> </div>
                         <div class="info_box_pos">
-                            <div> {{item.name}}</div>
-                            <div>  {{item.size[0]}}</div>
-                            <div> {{item.quantity[0]}}</div>
+                            <div class="inner_info_box"> 
+                                <div> {{item.name}}</div> 
+                            </div>
+                            <div> <span> Small </span> </div>
                         </div>
                         <div class="price_box_pos">
                             <div> {{item.price}}</div>
                         </div>
+                        <div class="quantity_box_pos">
+                            <span>{{item.quantity[0]}}</span> 
+                        </div>
+                        <div class="delievery_fee_box_pos">
+                            <span>무료</span>
+                        </div>
+                        <div class="actual_price_box_pos">
+                            <span>{{item.price * item.quantity[0]}}</span>
+                        </div>
+                        <div class="choose_box_pos">
+                            <div>
+                                <span></span>
+                                <img src="@/images/etc/button_delete.gif" class="delete_box" alt="delete button" @click="removeItem(item.id)">
+                            </div>
+                        </div>
                     </div>
-                    <button @click="removeItem(item.id)"></button>
                 </div>
                 <div v-if="item.quantity[1]" class="outer_product">
                     <div class="inner_product">
                         <div class="check_box_pos"> <input type="checkbox" class="check_box"></div>
-                        <div> <img :src="item.image" class="inner_img" alt=""> </div>
-                        <div> {{item.name}}</div>
-                        <div> {{item.price}}</div>
-                        <div> {{item.quantity[1]}}</div>
-                        <div> {{item.size[1]}}</div>
+                        <div class="image_box_pos"> <img :src="item.image" class="inner_img" alt=""> </div>
+                        <div class="info_box_pos">
+                            <div class="inner_info_box"> 
+                                <div> {{item.name}}</div> 
+                            </div>
+                            <div> <span> Medium </span> </div>
+                        </div>
+                        <div class="price_box_pos">
+                            <div> {{item.price}}</div>
+                        </div>
+                        <div class="quantity_box_pos">
+                            <span>{{item.quantity[1]}}</span> 
+                        </div>
+                        <div class="delievery_fee_box_pos">
+                            <span>무료</span>
+                        </div>
+                        <div class="actual_price_box_pos">
+                            <span>{{item.price * item.quantity[1]}}</span>
+                        </div>
+                        <div class="choose_box_pos">
+                            <div>
+                                <span></span>
+                                <img src="@/images/etc/button_delete.gif" class="delete_box" alt="delete button" @click="removeItem(item.id)">
+                            </div>
+                        </div>
                     </div>
-                    <button @click="removeItem(item.id)"></button>
                 </div>
             </div>
             <div>
@@ -76,17 +143,29 @@ function removeItem(id:number){
     justify-content: center;
 }
 .main_wrap{
-    width: 1000px;
-    background-color: green;
+    width: 1100px;
+    
     margin-top: 100px;
 }
+/* outer text area start*/
 .outer_text_area{
     width: 100%;
     display: flex;
+    flex-direction: column;
 }
 .material-icons.md-48{
     font-size: 48px;
 }
+.top_outer_text{
+    width: 100%;
+    display: flex;
+}
+.bot_outer_text{
+    width: 100%;
+    height: 40px;
+    display: flex;
+}
+/* outer text area end*/
 .inner_text{
     display: flex;
     align-items: center;
@@ -96,17 +175,90 @@ function removeItem(id:number){
 }
 .inner_product{
     width: 100%;
-    background-color: brown;
+    box-sizing: border-box;
+    border-bottom: 0.5px solid rgba(0, 0, 0, 0.2);
+    padding-top: 10px;
+    padding-bottom: 10px;
+    
     display: flex;
 }
 .inner_img{
-    width: 100px;
+    box-sizing: border-box;
+    width: 100%;
 }
+/*pos boxes start*/
 .check_box_pos{
+    width: 30px;
     display: flex;
+    justify-content: center;
+    align-items: center;
 }
+.image_box_pos{
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.info_box_pos{
+    width: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.inner_info_box{
+    display: flex;
+    justify-content: center;
+    margin-right: 30px;
+    align-items: center;
+}
+.price_box_pos{
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.quantity_box_pos{
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.delievery_fee_box_pos{
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.actual_price_box_pos{
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.choose_box_pos{
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+/*pos boxes end*/
 .check_box{
-    margin-left: 2px;
-    margin-right: 2px;
+}
+.delete_box{
+    cursor: pointer;
+}
+.cart_icon{
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.cart_text{
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
 }
 </style>
