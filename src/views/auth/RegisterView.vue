f2fa12c8854210a460cc9788f1690f4c1f711d95
jkkoh <jeongkyu7@gmail.com>
2023-07-05 19:06:36 +0900

    fixing mobile

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -57,39 +57,39 @@
             <p v-if="errorMessage" class="error-message text-danger mb-4">{{ errorMessage }}</p>
             <div class="m_inner_box">
               <label for="email" class="form-label m_label_title">이메일</label>
-              <input v-model="registerData.email" type="email" class="form-control" id="email" autocomplete="off" placeholder="" style="border: none;">
+              <input v-model="registerData.email" type="email" class="form-control m_input" id="email" autocomplete="off" placeholder="" style="border: none;">
             </div>
             <div class="m_inner_box">
               <label for="name" class="form-label m_label_title">이름</label>
-              <input v-model="registerData.name" type="text" class="form-control" id="first_name" autocomplete="off" placeholder="" style="border: none;">
+              <input v-model="registerData.name" type="text" class="form-control m_input" id="first_name" autocomplete="off" placeholder="" style="border: none;">
             </div>
             <div class="m_inner_box">
               <label for="password" class="form-label m_label_title">비밀번호</label>
-              <input v-model="registerData.password" type="password" class="form-control" id="password" placeholder="" style="border: none;">
+              <input v-model="registerData.password" type="password" class="form-control m_input" id="password" placeholder="" style="border: none;">
             </div>
             <div class="m_inner_box">
               <label for="password_confirm" class="form-label m_label_title">비밀번호 확인</label>
-              <input v-model="registerData.password_confirm" type="password" class="form-control" id="password_confirm" placeholder="" style="border: none;">
+              <input v-model="registerData.password_confirm" type="password" class="form-control m_input" id="password_confirm" placeholder="" style="border: none;">
             </div>
             <div class="m_inner_box">
-              <label for="postCode" class="form-label m_label_title">우편번호</label> <button class="m_search_btn" @click="sample4_execDaumPostcode()" style="width: 15vw; marginLeft: 10px; border: none; backgroundColor: rgba(100, 255, 71, 0.8);">검색</button>
-              <input v-model="registerData.postCode" type="postCode" class="form-control" id="postCode" autocomplete="off" placeholder="" style="width: 40vw; border:none;" > 
+              <label for="postCode" class="form-label m_label_title">우편번호</label> <button class="m_search_btn" @click="sample4_execDaumPostcode()">검색</button>
+              <input v-model="registerData.postCode" type="postCode" class="form-control m_input" id="postCode" autocomplete="off" placeholder="" style="width: 40vw; border:none;" > 
             </div>
             <div class="m_inner_box">
               <label for="address" class="form-label m_label_title">도로명 주소</label>
-              <input v-model="registerData.address" type="address" class="form-control" id="address" autocomplete="off" placeholder="" style="border: none;">
+              <input v-model="registerData.address" type="address" class="form-control m_input" id="address" autocomplete="off" placeholder="" style="border: none;">
             </div>
             <div class="m_inner_box">
               <label for="address1" class="form-label m_label_title">지번 주소</label>
-              <input v-model="registerData.address1" type="address1" class="form-control" id="address1" autocomplete="off" style="border: none;">
+              <input v-model="registerData.address1" type="address1" class="form-control m_input" id="address1" autocomplete="off" style="border: none;">
             </div>
             <div class="m_inner_box">
               <label for="address2" class="form-label m_label_title">상세주소</label>
-              <input v-model="registerData.address2" type="address2" class="form-control" id="address2" autocomplete="off" style="border: none;">
+              <input v-model="registerData.address2" type="address2" class="form-control m_input" id="address2" autocomplete="off" style="border: none;">
             </div>
             <div class="m_inner_box">
               <label for="address3" class="form-label m_label_title">참고항목</label>
-              <input v-model="registerData.address3" type="address3" class="form-control" id="address3" autocomplete="off" style="border: none;">
+              <input v-model="registerData.address3" type="address3" class="form-control m_input" id="address3" autocomplete="off" style="border: none;">
             </div>
             <button class="m_success_btn" @click="register()">Register</button>
           </div>
@@ -230,15 +230,13 @@ export default defineComponent({
   height: 20vw;
 }
 .m_label_title{
-  font-size: 20px;
+  font-size: 0.8rem;
 }
 .m_search_btn{
   width: 15vw;
   margin-left: 10px;
-  border: none;
-  border-radius: 20px;
-  background-color: rgba(100, 255, 71, 0.8);
-  color: white;
+  border: 1px solid black;
+  font-size: 0.8rem;
 }
 .m_success_btn{
   width: 90vw;
@@ -250,5 +248,10 @@ export default defineComponent({
   font-size: 25px;
   font-weight: 500;
   color: white;
+}
+.m_input{
+  background-color: #f5f5f5;
+  font-size: 0.8rem;
+  font-weight: 300;
 }
 </style>
